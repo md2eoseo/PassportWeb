af87a9f0ae664b254d64b4bd633c298bd9f22808
md2eoseo <md2eoseo@gmail.com>
2019-11-05 20:43:31 +0900

    express.js 폴더 셋업

--- express-styl/app.js
+++ express-styl/app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('dev')); // 각각의 요청에 대한 모든 log 출력
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
