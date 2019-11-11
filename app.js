3757901a4e5f9ab233b29753c8254522314c47b7
md2eoseo <md2eoseo@gmail.com>
2019-11-11 11:51:27 +0900

    jade -> pug

--- app.js
+++ app.js
@@ -5,21 +5,44 @@ var path = require('path');
 
 // Express.js 객체 초기화
 var app = express();
+app.locals.pretty = true;
 
 // app setting
 app.set('appName', 'passport-web');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 // middleware 정의
 
 // route 정의
-app.all('*', function(req, res){
+app.get('/', function(req, res){
     res.render('index', { msg: 'Passport!' });
 });
 
+app.get('/index', function(req, res){
+    res.render('index', { msg: 'Passport!' });
+});
+
+app.get('/post', function(req, res){
+    res.render('post', { msg: 'Passport!' });
+});
+
+app.get('/admin', function(req, res){
+    res.render('admin', { msg: 'Passport!' });
+});
+
+app.get('/login', function(req, res){
+    res.render('login', { msg: 'Passport!' });
+});
+
+app.get('/logout', function(req, res){
+    res.render('logout', { msg: 'Passport!' });
+});
+
+
 // 서버 시작
 http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port') + '.');
 });
