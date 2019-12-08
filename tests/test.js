749162f4bdfd4e72862424f5b1099c84c5b91cb2
md2eoseo <md2eoseo@gmail.com>
2019-12-08 19:32:02 +0900

    불필요 파일 삭제

--- tests/test.js
+++
@@ -1,7 +0,0 @@
-var assert = require('chai').assert;
-
-describe('String#split', function(){
-    it('should return an array', function(){
-        assert(Array.isArray('a,b,c'.split(',')));
-    });
-});
